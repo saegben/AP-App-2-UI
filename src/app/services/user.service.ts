701faf709a4de7db31bdedaceed141f86889e146
saegben <sagebened@gmail.com>
2022-09-09 14:39:03 -0500

    changed observable url

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,7 +8,7 @@ export class UserService {
     constructor(private http: HttpClient) {}
 
     observe(): Observable<any> {
-        let url = "http://localhost:4200/";
+        let url = "http://localhost:8080/";
         return this.http.get<any>(url);
     }
 }
